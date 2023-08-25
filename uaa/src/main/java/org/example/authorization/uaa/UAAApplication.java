511370ec17c0aebe21160799d1ff366013c25689
Monody12 <qq614908309@gmail.com>
2023-08-25 16:22:47 +0800

    project init

---
+++ uaa/src/main/java/org/example/authorization/uaa/UAAApplication.java
@@ -0,0 +1,11 @@
+package org.example.authorization.uaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UAAApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UAAApplication.class, args);
+    }
+}
